545028c5c3f15cd9a15309a3ed8caeb3260b4253
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-12-26 22:53:04 +0900

    header section 수정 및 Scroll header 추가

--- assets/main.js
+++ assets/main.js
@@ -16,3 +16,11 @@ searchInput.addEventListener('blur', function() {
     search.classList.remove('focused');
     searchInput.setAttribute('placeholder', '');
 });
+
+/* ==== SCROLL HEADER ==== */
+
+window.onscroll = () => {
+    const header = document.getElementById('header');
+    if(this.scrollY >= 200) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
+}
