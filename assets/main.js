aac4a0094c356522c71a9a9970a90de3901cf021
kod0751 <50727457+kod0751@users.noreply.github.com>
2021-12-27 13:40:08 +0900

    토글 메뉴 구현

--- assets/main.js
+++ assets/main.js
@@ -24,3 +24,18 @@ window.onscroll = () => {
     if(this.scrollY >= 200) header.classList.add('scroll-header');
     else header.classList.remove('scroll-header');
 }
+
+/* ==== SHOW MENU ==== */
+
+const showMenu = ( toggleId, navId ) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if( toggle && nav ) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav__toggle', 'nav__menu');
