f71012781298d3caf1696163016592d4915154f8
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-01-04 22:34:19 +0900

    menu linkAction 추가 및 디자인 수정

--- assets/main.js
+++ assets/main.js
@@ -65,3 +65,25 @@ function RemoveMenu() {
 }
 
 navLink.forEach( n => n.addEventListener('click', RemoveMenu))
+
+
+/*==== SCROLL SECTIONS ACTIVE LINK ====*/
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active')
+        }
+    })
+}
